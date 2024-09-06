1e3e3bbf67e712708c46729e6c2f08d3666e5726
Shah Ehsan <shahehsan2002@gmail.com>
2024-09-06 23:15:12 +0600

    Created Academic Faculty validation

---
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const academicFacultyValidationSchema = z.object({
+  name: z
+    .string({
+      invalid_type_error: 'Academic Faculty must be string',
+    })
+   
+});
+
+export const UserValidation = {
+  academicFacultyValidationSchema,
+};
