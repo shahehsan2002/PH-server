807f7efec9106d309f6daf131d4aab4724ba29eb
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-08 01:37:53 +0600

    updated some code in academic faculty validation file

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,16 +1,20 @@
 import { z } from "zod";
 
-const createAcademicFacultyValidationSchema = z.object({
-  name:z.string({
-      invalid_type_error: 'Academic Faculty must be string',
-  }),
+const createAcademicFacultyValidationSchema =z.object({
+  body: z.object({
+    name:z.string({
+        invalid_type_error: 'Academic Faculty must be string',
+    }),
+  })
 });
 
 
-const updateAcademicFacultyValidationSchema = z.object({
-  name:z.string({ 
-      invalid_type_error: 'Academic Faculty must be string',  
-  }).optional(),
+const updateAcademicFacultyValidationSchema =z.object({
+  body: z.object({
+    name:z.string({
+        invalid_type_error: 'Academic Faculty must be string',
+    }),
+  })
 });
 
 export const AcademicFacultyValidation = {
