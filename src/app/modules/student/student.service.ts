2ab4942ddfb352542103347446dcbeb53b0fc103
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-10 17:23:16 +0600

    Populate referencing fields

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -2,13 +2,25 @@ import { Student } from './student.model';
 
 const getAllStudentsFromDB = async () => {
   const result = await Student.find()
-  .populate('admissionSemester')
-  .populate('academicDepartment');
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
 
   return result;
 };
 const getSingleStudentFromDB = async (id: string) => {
-  const result = await Student.aggregate([{ $match: { id } }]);
+  const result = await Student.findById(id)
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
   return result;
 };
 
