a62945660617d221d8bc7735cb63f384375d36fd
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-10 20:17:32 +0600

    Delete student using transaction & rollback

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,4 +1,8 @@
+import mongoose from 'mongoose';
 import { Student } from './student.model';
+import AppError from '../../errors/AppError';
+import httpStatus from 'http-status';
+import { User } from '../user/user.model';
 
 const getAllStudentsFromDB = async () => {
   const result = await Student.find()
@@ -25,9 +29,54 @@ const getSingleStudentFromDB = async (id: string) => {
 };
 
 const deleteStudentFromDB = async (id: string) => {
-  const result = await Student.updateOne({ id }, { isDeleted: true });
-  return result;
+  const session = await mongoose.startSession();
+  try {
+    // Start the transaction
+    session.startTransaction();
+
+    // Soft-delete the student by setting isDeleted to true
+    const deletedStudent = await Student.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session }
+    );
+
+    // If student is not found, throw an error
+    if (!deletedStudent) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete student or student not found');
+    }
+
+    // Soft-delete the corresponding user by setting isDeleted to true
+    const deletedUser = await User.findOneAndUpdate(
+      { id },
+      { isDeleted: true },
+      { new: true, session }
+    );
+
+    // If user is not found, throw an error
+    if (!deletedUser) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user or user not found');
+    }
+
+    // Commit the transaction
+    await session.commitTransaction();
+    
+    // End the session
+    await session.endSession();
+
+    // Return the deleted student
+    return deletedStudent;
+
+  } catch (error) {
+    // Abort the transaction if there's an error
+    await session.abortTransaction();
+    await session.endSession();
+
+    // Propagate the error
+    throw error;
+  }
 };
+
 
 export const StudentServices = {
   getAllStudentsFromDB,
