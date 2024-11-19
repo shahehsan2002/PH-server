8ef42d6c61690edae99594800fec55575b5ebfc8
Shah Ehsan <shahehsan2002@gmail.com>
2024-11-19 19:42:24 +0600

    Error request Handler added for clean the code

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -2,15 +2,10 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode ||  500;
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
   const message = err.message || 'Something went wrong!';
 
   return res.status(statusCode).json({
@@ -21,3 +16,15 @@ const globalErrorHandler = (
 };
 
 export default globalErrorHandler;
+
+// Pattern for error handling
+
+/*
+success
+message
+errorSources:[
+path: ''
+message: ''
+]
+stack
+*/
