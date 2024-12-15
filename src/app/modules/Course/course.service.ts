c409ddf8bf0dc980ae31a2190c5b490ab46614e8
Shah Ehsan <shahehsan2002@gmail.com>
2024-12-15 22:10:01 +0600

    delete course services created

--- src/app/modules/Course/course.service.ts
+++ src/app/modules/Course/course.service.ts
@@ -1,22 +1,31 @@
-import { Course } from "./course.model"
+import { Course } from './course.model';
 
 const createCourseIntoDB = async () => {
-    const result = await Course.create();
-    return result;
-}
+  const result = await Course.create();
+  return result;
+};
 
 const getAllCoursesFromDB = async () => {
-    const result = await Course.find();
-    return result;
-}
+  const result = await Course.find();
+  return result;
+};
 
-const getSingleCourseFromDB = async (id:string) => {
-    const result = await Course.findById(id);
-    return result;
-}
+const getSingleCourseFromDB = async (id: string) => {
+  const result = await Course.findById(id);
+  return result;
+};
 
+const deleteCourseIntoDB = async (id: string) => {
+  const result = await Course.findByIdAndUpdate(
+    id,
+    { isDeleted: true },
+    { new: true }
+  );
+  return result;
+};
 export const CourseServices = {
-    createCourseIntoDB,
-    getAllCoursesFromDB,
-    getSingleCourseFromDB,
-}
+  createCourseIntoDB,
+  getAllCoursesFromDB,
+  getSingleCourseFromDB,
+  deleteCourseIntoDB,
+};
