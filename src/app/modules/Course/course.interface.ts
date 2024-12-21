45280948e926d37f0b7cd5c31664872f66ec5dc8
Shah Ehsan <shahehsan2002@gmail.com>
2024-12-21 13:24:36 +0600

    course faculty model created

--- src/app/modules/Course/course.interface.ts
+++ src/app/modules/Course/course.interface.ts
@@ -14,7 +14,7 @@ export type TCourse = {
   preRequisiteCourses: [TPreRequisiteCourses];
 };
 
-export type CourseFaculty = {
+export type TCourseFaculty = {
   course: Types.ObjectId;
   faculties: [Types.ObjectId];
 };
--- src/app/modules/Course/course.model.ts
+++ src/app/modules/Course/course.model.ts
@@ -1,5 +1,9 @@
 import { model, Schema } from 'mongoose';
-import { TCourse, TPreRequisiteCourses } from './course.interface';
+import {
+  TCourse,
+  TCourseFaculty,
+  TPreRequisiteCourses,
+} from './course.interface';
 
 const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
   course: {
@@ -41,5 +45,23 @@ const courseSchema = new Schema<TCourse>({
   },
 });
 
-
 export const Course = model<TCourse>('Course', courseSchema);
+
+const courseFacultySchema = new Schema<TCourseFaculty>({
+  course: {
+    type: Schema.Types.ObjectId,
+    ref: 'Course',
+    unique: true,
+  },
+  faculties: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Faculty',
+    },
+  ],
+});
+
+export const CourseFaculty = model<TCourseFaculty>(
+  'CourseFaculty',
+  courseFacultySchema
+);
