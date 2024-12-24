00b23c2085422479b9cf7fabe388fa72fe35f51f
Shah Ehsan <shahehsan2002@gmail.com>
2024-12-24 20:21:17 +0600

    created create offered course route

---
+++ src/app/modules/OfferedCourse/OfferedCourse.route.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { OfferedCourseValidations } from './OfferedCourse.validation';
+import { OfferedCourseController } from './OfferedCourse.controller';
+
+const router = express.Router();
+
+// router.get('/', OfferedCourseController.getAllOfferedCourses);
+
+// router.get('/:id', OfferedCourseControllers.getSingleOfferedCourses);
+
+router.post(
+  '/create-offered-course',
+  validateRequest(OfferedCourseValidations.createOfferedCourseValidationSchema),
+  OfferedCourseController.createOfferedCourse,
+);
+
+// router.patch(
+//   '/:id',
+//   validateRequest(OfferedCourseValidations.updateOfferedCourseValidationSchema),
+//   OfferedCourseControllers.updateOfferedCourse,
+// );
+
+// router.delete(
+//   '/:id',
+//   OfferedCourseControllers.deleteOfferedCourseFromDB,
+// );
+
+export const offeredCourseRoutes = router;
