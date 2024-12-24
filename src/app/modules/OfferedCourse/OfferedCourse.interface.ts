73eb32a8d585308c5d69df744e9d5cad00b1ca1a
Shah Ehsan <shahehsan2002@gmail.com>
2024-12-24 19:15:07 +0600

    OfferedCourse model created

--- src/app/modules/OfferedCourse/OfferedCourse.interface.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.interface.ts
@@ -1,13 +1,14 @@
 import { Types } from 'mongoose';
 
-export type TDays = 'sat' | 'sun' | 'mon' | 'tue' | 'wed' | 'thu' | 'fri';
+export type TDays = 'Sat' | 'Sun' | 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri';
 
 export type TOfferedCourse = {
   semesterRegistration: Types.ObjectId;
   academicSemester?: Types.ObjectId;
-  academicFaculty?: Types.ObjectId;
-  course?: Types.ObjectId;
-  faculty?: Types.ObjectId;
+  academicFaculty: Types.ObjectId;
+  academicDepartment: Types.ObjectId;
+  course: Types.ObjectId;
+  faculty: Types.ObjectId;
   maxCapacity: number;
   section: number;
   days: TDays[];
@@ -16,7 +17,7 @@ export type TOfferedCourse = {
 };
 
 export type TSchedule = {
-  day: TDays[];
+  days: TDays[];
   startTime: string;
   endTime: string;
 };
