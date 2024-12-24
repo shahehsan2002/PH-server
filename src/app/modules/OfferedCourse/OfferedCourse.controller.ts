185b52c6edbf99620ddd8e68c9c6305aeb64740e
Shah Ehsan <shahehsan2002@gmail.com>
2024-12-24 20:15:12 +0600

    create offered course service created

--- src/app/modules/OfferedCourse/OfferedCourse.controller.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.controller.ts
@@ -2,19 +2,19 @@ import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import { OfferedCourseService } from './OfferedCourse.service';
 
 const createOfferedCourse = catchAsync(async (req: Request, res: Response) => {
   const result = await OfferedCourseService.createOfferedCourseIntoDB(req.body);
 
-  sendResponse(res,{
-    statusCode:httpStatus.OK,
-    success:true,
-    message:'Offered course is created successfully',
-    data:result
-  })
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Offered course is created successfully',
+    data: result,
+  });
 });
-
 
 export const OfferedCourseController = {
   createOfferedCourse,
-}
+};
