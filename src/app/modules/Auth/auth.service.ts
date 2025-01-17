7de857282c738e3a45aa76aa37924b97bdc3c015
Shah Ehsan <shahehsan2002@gmail.com>
2025-01-17 21:54:29 +0600

    updated user interface

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,6 +2,7 @@ import httpStatus from 'http-status';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
   // Checking if user exists
@@ -11,19 +12,23 @@ const loginUser = async (payload: TLoginUser) => {
   }
 
   //   checking if the user is already deleted
-
   const isDeleted = isUserExists?.isDeleted;
   if (isDeleted) {
     throw new AppError(httpStatus.FORBIDDEN, 'User is deleted');
   }
 
   //   checking if the user is blocked
-
   const userStatus = isUserExists?.status;
   if (userStatus === 'blocked') {
     throw new AppError(httpStatus.FORBIDDEN, 'User is blocked');
   }
 
+  //   checking if the password is correct
+  const isPasswordMatched = await bcrypt.compare(
+    payload?.password,
+    isUserExists?.password
+  );
+  console.log(isPasswordMatched);
   return {};
 };
 
