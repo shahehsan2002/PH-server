3449fa478b69f7a2c4226116f7cc774750ea549b
Shah Ehsan <shahehsan2002@gmail.com>
2025-02-16 21:15:40 +0600

    updated implemet static for validation

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,32 +2,47 @@ import httpStatus from 'http-status';
 import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
+import jwt from 'jsonwebtoken';
+import config from '../../config';
 
 const loginUser = async (payload: TLoginUser) => {
   // Checking if user exists
-
-  const user  = await User.isUserExistsByCustomId(payload.id);
+  const user = await User.isUserExistsByCustomId(payload.id);
 
   if (!user) {
     throw new AppError(httpStatus.NOT_FOUND, 'User not found');
   }
 
-  //   checking if the user is already deleted
-  // const isDeleted = isUserExists?.isDeleted;
-  // if (isDeleted) {
-  //   throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted');
-  // }
-
-  //   checking if the user is blocked
-  // const userStatus = isUserExists?.status;
-  // if (userStatus === 'blocked') {
-  //   throw new AppError(httpStatus.FORBIDDEN, 'User is blocked !');
-  // }
-
-  //   checking if the password is correct
-  if(!(await User.isPasswordMatched(payload.password,user?.password))){
-    throw new AppError(httpStatus.UNAUTHORIZED, 'Invalid password');
+  // Checking if the user is already deleted
+  if (user.isDeleted) {
+    throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted');
   }
+
+  // Checking if the user is blocked
+  if (user.status === 'blocked') {
+    throw new AppError(httpStatus.FORBIDDEN, 'User is blocked!');
+  }
+
+  // Checking if the password is correct
+  const isPasswordMatched = await User.isPasswordMatched(payload.password, user.password);
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.UNAUTHORIZED, 'Password is incorrect');
+  }
+
+  // Create token and send to user
+  const jwtPayload = {
+    userId: user, // Use user._id instead of the whole user object
+    role: user.role,
+  };
+
+  const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret as string, {
+    expiresIn: '10d',
+  });
+
+  return {
+    accessToken,
+    needsPasswordChange: user.needsPasswordChange,
+  };
 };
 
 export const AuthServices = {
